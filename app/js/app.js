c40d41570b3c3c75517e1beb224e02257b53e369
Dustin Gulley <dlgulley411@gmail.com>
2016-03-06 09:49:17 -0500

    enhancement: Adding button to switch between map and schedule views
    - Added button to switch between map and schedule views
    - Added google maps but still need an api key to use it
    
    resolves: none

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,37 @@
 	'use strict';
 
 	var _app = document.querySelector('#app');
-	_app.selected = 0;
 
 	// See https://github.com/Polymer/polymer/issues/1381
   	window.addEventListener('WebComponentsReady', function() {
 
+		/**
+		 * Selected view
+		 * @type {number}
+		 */
   		_app.selected = 0;
+
+  		/**
+		 * Show the map view
+		 * @function viewMap
+		 *
+		 */
+  		_app.viewMap = function() {
+
+  			app.selected = 0;
+
+  		};
+
+  		/**
+		 * Show the schedule view
+		 * @function viewSchedule
+		 *
+		 */
+  		_app.viewSchedule = function () {
+
+  			_app.selected = 1;
+
+  		};
   		
   	});
 
